0c5f87261fec49b10e4c065b5ee7c412798e8018
Rane Wallin <github@blasph.me>
2019-05-12 19:13:55 +0000

    userschema

---
+++ userSchema.js
@@ -0,0 +1,45 @@
+{
+    siteTitle: "string",
+    name: "string",
+    subHeading: "string",
+    email: "string",
+    phone: "string",
+    bio: "string",
+    id: "oauthID",
+    awards: [{
+        awardID: "number",
+        awardName: "string",
+        awardDate: "string",
+        awardDescription: "string"
+    }],
+    education: [{
+        programID: "number",
+        schoolName: "string",
+        program: "string",
+        notes: "string"
+    }],
+    projects: [{
+        projectID: "number",
+        projectName: "string",
+        github: "url",
+        hosted: "url",
+        tags: [],
+        description: "string",
+        timeframe: "string"
+    }],
+    posts: [{
+        postID: "number",
+        postTitle: "string",
+        createdOn: "date",
+        updatedOn: "date",
+        username: "string",
+        tags: [],
+        body: "string",
+        comments: [{
+            commentName: "string",
+            commentEmail: "string",
+            commendDate: "date",
+            commentContent: "string"
+        }]
+    }]
+}
