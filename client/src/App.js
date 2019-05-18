45edb623b90a642e19fffaebcfa2a6a1e017b18b
RaneWallin <rane@ranewallin.com>
2019-05-18 14:31:56 -0700

    style header

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 import Header from "./components/Header";
 import Home from "./components/Home";
@@ -7,14 +8,21 @@ import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
-    <div className="container">
-      <Router>
-        <div>
+    <div>
+      <Helmet bodyAttributes={{ class: "grey lighten-2" }} />
+      <div>
+        <div className="deep-purple darken-4">
           <Header />
-          <Route path="/" exact component={Home} />
-          <Route path="/Dashboard" component={Dashboard} />
         </div>
-      </Router>
+        <div className="container">
+          <Router>
+            <div>
+              <Route path="/" exact component={Home} />
+              <Route path="/Dashboard" component={Dashboard} />
+            </div>
+          </Router>
+        </div>
+      </div>
     </div>
   );
 }
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="navbar-fixed">
+      <nav className="deep-purple darken-4">
+        <div className="container">
+          <div className="nav-wrapper">
+            <ul className="left">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Achievements</a>
+              </li>
+              <li>
+                <a href="#">Projects</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
