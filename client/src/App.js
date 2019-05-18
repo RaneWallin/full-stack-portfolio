8fb35cefbfb1c6d1e8518189ff8da5102af8eaf5
RaneWallin <rane@ranewallin.com>
2019-05-18 14:06:35 -0700

    scaffold in Header, Home, and Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Dashboard from "./components/Dashboard";
 
 function App() {
-  return <div className="container">Hello</div>;
+  return (
+    <div className="container">
+      <Router>
+        <div>
+          <Header />
+          <Route path="/" exact component={Home} />
+          <Route path="/Dashboard" component={Dashboard} />
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
